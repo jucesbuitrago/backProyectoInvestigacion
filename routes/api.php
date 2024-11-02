6fc57f4262ae020b0d756f2f0e0fe5ecc84b7911
Jucesbuitrago <cesarbuitrago78@gmail.com>
2024-11-02 17:09:34 -0500

    Proyecto base

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('logout',[AuthController::class,'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('/users',UserController::class);
 });
+
+
+
+Route::post('login',[AuthController::class,'login']);
+Route::post('register',[AuthController::class,'register']);
